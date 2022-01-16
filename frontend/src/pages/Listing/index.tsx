c2c370f0949e368ad64f60671178b25b158a8c38
wslmacieira <wslmacieira@outlook.com>
2022-01-15 21:58:03 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,24 @@
 import axios from 'axios';
 import MovieCard from 'components/MovieCard';
 import Pagination from 'components/Pagination';
+import { useState } from 'react';
+import { MoviePage } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 
 function Listing() {
 
+  const [pageNumber, setPageNumber] = useState(0);
+
   // forma errada
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+  axios.get(`${BASE_URL}/movies?size=12&page=1`)
     .then(response => {
-      console.log(response.data)
+      const data = response.data as MoviePage;
+      console.log(data)
+      setPageNumber(data.number);
     })
   return (
     <>
+    <p>{pageNumber}</p>
       <Pagination />
       <div className="contanier">
         <div className="row">
